78133a0459214179e2bd7101ac0c10afdd46bacc
GitHub User <user@example.com>
2025-09-24 14:46:21 +0800

    do not run cicd auto

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
-export default defineConfig({
-  // 设置应用的基础路径，确保在子路径下正确运行
-  base: '/project/lowcode/',
-  plugins: [react(),tailwindcss()],
-})
+export default defineConfig(({mode}) => {
+  const isProduction = mode === "production";
+  return {
+    // 设置应用的基础路径，确保在子路径下正确运行
+
+    base: isProduction?"/project/lowcode/":'/',
+    plugins: [react(), tailwindcss()],
+    build: {
+      outDir: "dist",
+      assetsDir: "assets",
+      // 确保静态资源路径正确生成
+    },
+  };
+});
